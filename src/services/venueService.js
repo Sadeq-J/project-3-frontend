e7560e1a94299adb7766670265ee08555f3dda1e
Sadiq-J <sadiq_b2@hotmail.com>
2026-08-11 11:26:50 +0300

    feat: create profile page

--- src/services/venueService.js
+++ src/services/venueService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL =
   import.meta.env.VITE_BACKEND_SERVER_URL || "http://localhost:3000";
 
-export const getVenues = async (filters = {}) => {
+const getVenues = async (filters = {}) => {
   const response = await axios.get(`${API_URL}/venues`, {
     params: filters,
   });
@@ -11,7 +11,7 @@ export const getVenues = async (filters = {}) => {
   return response.data;
 };
 
-export const getVenueById = async (id) => {
+const getVenueById = async (id) => {
   const response = await axios.get(`${API_URL}/venues/${id}`);
 
   return response.data;

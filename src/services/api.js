71762978845f3d47c09ab507a081e5295f9a1eb2
ALIALSAEED313 <202300573@student.polytechnic.bh>
2026-08-12 10:56:54 +0300

    Update API baseURL with default fallback
    Set default baseURL to 'http://localhost:3000' if not provided.

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:import.meta.env.VITE_BACK_END_SERVER_URL ,
+  baseURL: import.meta.env.VITE_BACK_END_SERVER_URL || "http://localhost:3000",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
+    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
 
